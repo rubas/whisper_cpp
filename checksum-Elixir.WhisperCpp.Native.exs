b53a93fe3756075888f42b1b335699f8eb1a26f0
Dominic Rubas <1042243+rubas@users.noreply.github.com>
2026-06-11 22:09:06 +0200

    chore: refresh NIF checksum for v0.4.0

--- checksum-Elixir.WhisperCpp.Native.exs
+++ checksum-Elixir.WhisperCpp.Native.exs
@@ -1,8 +1,8 @@
 %{
-  "libwhisper_cpp_native-v0.3.1-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:f8d0ae161156f9a724768994e77bf2952f7948699f854dabf0aff9ef643339cd",
-  "libwhisper_cpp_native-v0.3.1-nif-2.17-aarch64-unknown-linux-gnu--cuda.so.tar.gz" => "sha256:2657ea0d50768fd4bc3195f710a3f29b3c01cf096049e1eadc813f0dd48887c4",
-  "libwhisper_cpp_native-v0.3.1-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:37cec8717f3ee541ab9df00db054269d9b45d0e84ef3e8c6023b9d11c87da852",
-  "libwhisper_cpp_native-v0.3.1-nif-2.17-x86_64-unknown-linux-gnu--cuda.so.tar.gz" => "sha256:11b434c48819c8da24b1877f8c7d8a101d8e6630a51b9e185d8911e234783ac4",
-  "libwhisper_cpp_native-v0.3.1-nif-2.17-x86_64-unknown-linux-gnu--hipblas.so.tar.gz" => "sha256:f177914024d58f0bbeeccfbd2fc7aaa110919b96fcbaebfa3127bb810f5b0bd7",
-  "libwhisper_cpp_native-v0.3.1-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:7bbc3b9403d62b9ea5e3b009e3ff438a699884934ac65f8d622b9a8dfe946a83",
+  "libwhisper_cpp_native-v0.4.0-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:4f79d92dba324aaa05f9d09795a04d92b3f861c7d09f06964f217f653999607a",
+  "libwhisper_cpp_native-v0.4.0-nif-2.17-aarch64-unknown-linux-gnu--cuda.so.tar.gz" => "sha256:8067a91d0d3201b748fe4f2c61bbd67a4293b942cf93d67c2136c81c308154c6",
+  "libwhisper_cpp_native-v0.4.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:de5c846e87438a5ad90f22d8bb9a11711c0add51c154cb89f5fc04f4aa36e57b",
+  "libwhisper_cpp_native-v0.4.0-nif-2.17-x86_64-unknown-linux-gnu--cuda.so.tar.gz" => "sha256:80125f64522afa47af4124e171dbf2234853087b6fe5273ba56c26ab501ceae2",
+  "libwhisper_cpp_native-v0.4.0-nif-2.17-x86_64-unknown-linux-gnu--hipblas.so.tar.gz" => "sha256:c995881bc3000f7262eb9405ab9285660c5d5897d9216732397ec222ca201708",
+  "libwhisper_cpp_native-v0.4.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:2a470e1b2eecf65a41e5736d66b17121ebabeafd3c6bccd976c6194fae2dda07",
 }
