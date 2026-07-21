e53101db822c0a06f754b7932b69398addf9a2b3
Rubas <rubas@users.noreply.github.com>
2026-07-21 20:32:20 +0200

    chore(deps): update hex, cargo, and action dependencies (#33)
    Co-authored-by: Dominic Rubas <1042243+rubas@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -81,11 +81,11 @@ defmodule WhisperCpp.MixProject do
     [
       {:rustler_precompiled, "~> 0.9.0"},
       {:rustler, "~> 0.38.0", optional: true},
-      {:credo, "~> 1.7.18", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7.19", only: [:dev, :test], runtime: false},
       {:ex_slop, "~> 0.4", only: [:dev, :test], runtime: false},
       {:ex_dna, "~> 1.5", only: [:dev, :test], runtime: false},
       {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.40.2", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.40.3", only: :dev, runtime: false}
     ]
   end
 end
