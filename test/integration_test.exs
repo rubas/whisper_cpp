8ae26d5e5d8809a564667e793728837edd2889de
Dominic Rubas <1042243+rubas@users.noreply.github.com>
2026-09-22 23:47:11 +0200

    chore: move the vendored whisper.cpp to v1.9.4

--- test/integration_test.exs
+++ test/integration_test.exs
@@ -357,6 +357,11 @@ defmodule WhisperCpp.IntegrationTest do
     assert {:ok, %WhisperCpp.Transcription{}} =
              WhisperCpp.transcribe(model_ref, {:pcm_f32, short_pcm}, language: "en", n_threads: 4)
 
+    # One sample: shorter than the 200-sample reflect pad of the mel
+    # spectrogram. whisper.cpp before v1.9.3 read past the buffer here.
+    assert {:ok, %WhisperCpp.Transcription{segments: []}} =
+             WhisperCpp.transcribe(model_ref, {:pcm_f32, <<0.0::little-float-32>>}, language: "en")
+
     assert {:error, %WhisperCpp.Error{reason: :invalid_request}} =
              WhisperCpp.transcribe(model_ref, {:pcm_f32, ""}, language: "en")
   end
